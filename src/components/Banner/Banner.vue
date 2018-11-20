c39236891a3bd9970ec939c799fc02953c6d488c
HunterYoung <921349203@qq.com>
2018-11-20 22:49:38 +0800

    add banner

---
+++ src/components/Banner/Banner.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="banner">
+    <div class="topic-preview-wrapper">
+      <div class="topic-preview-list-wrapper">
+        <ul class="topic-preview" style="width: 500%;" ref="banner">
+          <BannerItem></BannerItem>
+        </ul>
+      </div>
+      <a class="more-topic">更多
+        <i class="b-icon"></i>
+      </a>
+      <div class="s-bottom">
+        <div class="title">
+            <span>Banner</span>
+        </div>
+        <ul class="slide-bar">
+          <li ></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BannerItem from './BannerItem.vue'
+export default {
+  name: 'Banner',
+  components: {
+    BannerItem
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Antd from 'ant-design-vue'
 import router from './router'
+import 'ant-design-vue/dist/antd.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Antd)
 
 /* eslint-disable no-new */
 new Vue({
