c85d730f058aaf8ff810a5173a00921b244b72d6
HunterYoung <921349203@qq.com>
2018-12-17 00:04:29 +0800

    modify showDetail page

--- src/main.js
+++ src/main.js
@@ -5,10 +5,14 @@ import App from './App'
 import Antd from 'ant-design-vue'
 import router from './router'
 import 'ant-design-vue/dist/antd.css'
+import VideoPlayer from 'vue-video-player'
+require('video.js/dist/video-js.css')
+require('vue-video-player/src/custom-theme.css')
 
 Vue.config.productionTip = false
 
 Vue.use(Antd)
+Vue.use(VideoPlayer)
 
 /* eslint-disable no-new */
 new Vue({
--- src/pages/ShowDetail/ShowDetail.vue
+++ src/pages/ShowDetail/ShowDetail.vue
@@ -1,6 +1,103 @@
 <template>
   <div class="container">
     <BannerBw></BannerBw>
+
+    <a-layout>
+      <a-layout-content>
+        <a-row class="video-introduction">
+          <a-col :span="6">{{videoName}}</a-col>
+          <a-col :span="6">{{videoTime}}</a-col>
+          <a-col :span="6">{{introduction}}</a-col>
+          <a-col :span="6"><button>展开</button></a-col>
+        </a-row>
+        <div class="video_list">
+        <div class="video_ls clear"  ref="videoLs">
+          <div class="video_shows" v-for="(item,index) in srclist">
+            <div v-if="item.imsrc" class="div_videos" >
+              <img :src="'../../static/images/'+item.imsrc+'.jpg'" :ref="index"/>
+            </div>
+            <div v-if="item.videosrc"  class="div_videos" >
+              <video-player class="video-player vjs-custom-skin"
+                            :ref="index"
+                            :options="playerOptions"
+                            :playsinline="true"
+                            @play="onPlayerPlay($event)"
+                            @pause="onPlayerPause($event)"
+                            @ended="onPlayerEnded($event)"
+                            @loadeddata="onPlayerLoadeddata($event)"
+                            @waiting="onPlayerWaiting($event)"
+                            @playing="onPlayerPlaying($event)"
+                            @timeupdate="onPlayerTimeupdate($event)"
+                            @canplay="onPlayerCanplay($event)"
+                            @canplaythrough="onPlayerCanplaythrough($event)"
+                            @ready="playerReadied"
+                            @statechanged="playerStateChanged($event)"
+              >
+              </video-player>
+            </div>
+          </div>
+        </div>
+      </div>
+        <a-row class="videoRelate">
+          <a-col :span="4" class="visit-number">
+            <img src="../../assets/images/videoDetail/visit-number.png" alt="visitIcon">
+            <span>{{visitNumber}}</span>
+          </a-col>
+          <a-col :span="4" class="nice-number">
+            <img src="../../assets/images/videoDetail/niced-icon.png" alt="visitNiceIcon">
+            <img src="../../assets/images/videoDetail/niced.png" alt="visitedNice">
+            <span>{{niceNumber}}</span>
+          </a-col>
+          <a-col :span="4" class="collect-number">
+            <img src="../../assets/images/videoDetail/collect-number.png" alt="visitCollect">
+            <img src="../../assets/images/videoDetail/collect-number-icon.png" alt="visitCollectIcon">
+            <span>{{collectNumber}}</span>
+          </a-col>
+          <a-col :span="4">
+            <img src="../../assets/images/videoDetail/share-icon.png" alt="shareIcon">
+            <a-button><img src="" alt=""></a-button>
+          </a-col>
+          <a-col :span="4">
+          </a-col>
+          <a-col :span="4">
+            <a-layout>
+              <a-layout-content>
+                <img src="../../assets/images/videoDetail/lighting.png" alt="lighting">
+                <a class="lighting-ta">电TA</a>
+              </a-layout-content>
+              <a-layout-footer>
+                <span>一个电池只能电一次哦~</span>
+              </a-layout-footer>
+            </a-layout>
+          </a-col>
+        </a-row>
+      </a-layout-content>
+      <a-layout-sider class="main-sider">
+        <a-layout class="user-about">
+          <a-layout-content class="user-about-content">
+            <a-layout class="userInfo">
+              <a-layout>
+                <a-layout-sider class="userInfo-avatar">
+                  <img src="../../assets/images/videoDetail/avatar.png" alt="avatar">
+                </a-layout-sider>
+                <a-layout-content class="userInfo-text">
+                  <a-layout>
+                    <a-layout-header><h2><a href="">{{nikeName}}</a></h2><span>lv2</span></a-layout-header>
+                    <a-layout-content><!--<img src="" alt="">--><span>厂牌</span></a-layout-content>
+                    <a-layout-footer><span>{{personalIntroduction}}</span></a-layout-footer>
+                  </a-layout>
+                </a-layout-content>
+              </a-layout>
+            </a-layout>
+          </a-layout-content>
+          <a-layout-footer>
+            <a-button>关注{{concernNumber}}</a-button>
+            <img src="../../assets/images/videoDetail/private-message.png" alt="privateMessage">
+          </a-layout-footer>
+        </a-layout>
+      </a-layout-sider>
+    </a-layout>
+
   </div>
 </template>
 
@@ -10,10 +107,170 @@
     name: 'ShowDetail',
     components: {
       BannerBw
+    },
+    data () {
+      return {
+        // videojs options
+        playerOptions: {
+          muted: false,//开启声音
+          autoplay: false,
+          language: 'en',
+          playbackRates: [0.7, 1.0, 1.5, 2.0],
+          sources: [{
+            type: "video/mp4",
+            src: "",
+          }],
+          poster: "https://surmon-china.github.io/vue-quill-editor/static/images/surmon-1.jpg",
+        },
+        refslist:[],//对象列表
+        srclist:[//视频图片列表
+          {videosrc:"http://vjs.zencdn.net/v/oceans.mp4"}
+        ],
+        num:0,
+        time:'',
+        videoName: '青鸟飞鱼',
+        videoTime: '2018-12-16',
+        introduction: '介绍',
+        visitNumber: '999+',
+        niceNumber: '999+',
+        collectNumber: '999+',
+
+        nikeName: '钓鱼的彩虹糖',
+        personalIntroduction: '没有个性就是我的个性',
+        concernNumber: 144
+      }
+    },
+    mounted() {
+      if(this.srclist[0].videosrc){//第一个是视频
+        this.playerOptions.sources[0].src=this.srclist[this.num].videosrc
+      }else{//第一个是图片
+        this.startPlay()
+      }
+    },
+    methods: {
+      startPlay(){//图片
+        this.time=setInterval(()=>{
+          this.countPlay();
+          if(this.srclist[this.num].videosrc){
+            clearInterval(this.time);
+            this.playerOptions.sources[0].src=''
+            this.playerOptions.sources[0].src=this.srclist[this.num].videosrc
+          }
+        },3000)
+      },
+      countPlay(){
+        this.num++;
+        this.$refs.videoLs.style.left=-600*(this.num)+"px";
+        this.$refs.videoLs.style.transition="2s"
+        if(this.num>=this.srclist.length){
+          this.num=0;
+          this.$refs.videoLs.style.left="0px"
+          this.$refs.videoLs.style.transition="0s"
+        }
+      },
+      onPlayerPlay(player) {
+      },
+      onPlayerPause(player) {
+      },
+      onPlayerEnded(player) {//播放完毕之后 src被替换
+        this.countPlay()
+        this.playerOptions.sources[0].src=''
+        this.playerOptions.sources[0].src=this.srclist[this.num].videosrc
+        if(this.srclist[this.num].imsrc){
+          this.startPlay()
+        }
+      },
+      onPlayerLoadeddata(player) {
+      },
+      onPlayerWaiting(player) {
+      },
+      onPlayerPlaying(player) {
+      },
+      onPlayerTimeupdate(player) {
+      },
+      onPlayerCanplay(player) {
+      },
+      onPlayerCanplaythrough(player) {
+      },
+      // or listen state event
+      playerStateChanged(playerCurrentState) { //播放的进程中就是一直在播放
+        // console.log('player current update state', playerCurrentState)
+      },
+      // player is ready 从第几秒开始播放
+      playerReadied(player) {
+        // seek to 10s
+        // console.log('example player 1 readied', player)
+        // player.currentTime(10)
+        // console.log('example 01: the player is readied', player)
+      }
     }
   }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  *
+    padding 0
+    margin 0
+  .clear
+    *zoom 1
+  .clear:after
+    visibility hidden
+    content ""
+    display block
+    clear both
+    height 0
+  .video-introduction
+    color black
+  .video_list
+    width 952px
+    height 400px
+    overflow hidden
+    position relative
+    margin 0 auto
+  .video_ls
+    width 500%
+    height 100%
+    position absolute
+    top 0
+    left 0
+    /*transition: 2s;*/
+  .video_shows
+    width 20%
+    height 100%
+    float left
+    /*transform: translate3d(-600px,0,0);*/
+  .div_videos
+    width 100%
+    height 100%
+  .div_videos img
+    width 100%
+    height 100%
+  .container
+    text-align center
+  /*.container .ant-layout-sider*/
+    /*background #fff*/
+    /*color #fff*/
+    /*line-height 120px*/
+  .container .ant-layout-content
+    background #fff
+    color #fff
+  .container > .ant-layout
+  .container > .ant-layout:last-child
+    margin 0
+  .videoRelate
+    color black
+    white-space normal nowrap
+  .lighting-ta
+    width 68px
+    height 27px
+    font-family MicrosoftYaHei-Bold
+    font-size 29.2px
+    font-weight normal
+    font-stretch normal
+    letter-spacing 0px
+    color #00a0e9
+  .concern
+    color #00a0e9
+  .userInfo
+    margin-left 0
 </style>
